e1b1b0e13f13045fdfcc7711c94f7acc3d6b8e00
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2016-05-29 14:35:39 -0400

    Initial commit

---
+++ timestamp.js
@@ -0,0 +1,25 @@
+const express = require('express'),
+      app = express();
+
+let portNumber;
+
+if (process.argv > 2) {
+  portNumber = process.argv[2];
+} else {
+  portNumber = 8080;
+}
+
+app.get('/api/:time', function(request, response) {
+  let timeObject = {},
+      time = request.params.time;
+  if (isNaN(time)) {
+    
+  } else {
+    time = parseInt(time);
+    timeObject["unix"] = time;
+    timeObject["natural"] = new Date(time*1000).toDateString(); // multiply by 1000 because usually counted in seconds
+  }
+  response.json(timeObject);
+});
+
+app.listen(portNumber);
