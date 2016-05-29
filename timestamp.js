7f125dc69dd2bd68dea4a9a79fb08edf3813d309
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2016-05-29 15:26:51 -0400

    Add repository and author field to package.js; add instructions at invlid page

--- timestamp.js
+++ timestamp.js
@@ -4,17 +4,32 @@ const express = require('express'),
 
 let portNumber = process.env.PORT || process.argv[2] || 8080;
 
-app.get('/api/:time', function(request, response) {
+app
+.get('/api/:timestamp', function(request, response) {
   let timeObject = {},
-      time = request.params.time;
-  if (isNaN(time)) {
-
+      datetime = request.params.timestamp;
+  if (isNaN(datetime)) {
+    let date = new Date(Date.parse(datetime));
+    if (date == "Invalid Date") {
+      timeObject["unix"] = null;
+      timeObject["natural"] = null;
+    } else {
+      timeObject["unix"] = date.getTime()/1000;
+      timeObject["natural"] = date.toDateString();
+    }
   } else {
-    time = parseInt(time);
-    timeObject["unix"] = time;
-    timeObject["natural"] = new Date(time*1000).toDateString(); // multiply by 1000 because usually counted in seconds
+    datetime = parseInt(datetime);
+    timeObject["unix"] = datetime;
+    timeObject["natural"] = new Date(datetime*1000).toDateString(); // multiply by 1000 because usually counted in seconds
   }
   response.json(timeObject);
+})
+.get('*', function (request, response) {
+  let responseString = "Please use web server as follows:\n" +
+      "Go to https://aryanj-unix-converter.herokuapp.com/api/[timestamp]\n" +
+      "where [timestamp] is a unix or natural language timestamp.\n" + 
+      "This app returns null values in the case of invalid timestamps.";
+  response.end(responseString);
 });
 
 app.listen(portNumber);
